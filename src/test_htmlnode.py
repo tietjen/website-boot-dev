3da1423f4cf67963a8a18c7c8c0a3da7791f5cb5
tietjen <jan.tietjen@parasec.de>
2025-05-11 14:54:24 +0200

    3rd HTMLNode test case added.

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -11,3 +11,7 @@ class TestHtmlNode(unittest.TestCase):
     def test_props_to_html(self):
         node = HTMLNode("a", "Test", None, {"href": "https://www.google.com", "a": "https:example.com"})
         self.assertEqual(' href="https://www.google.com" a="https:example.com"', node.props_to_html())
+        
+    def test_empty(self):
+        node = HTMLNode()
+        self.assertEqual("HTMLNode(tag: 'None', value: 'None', children: 'None', props: 'None')", repr(node))
